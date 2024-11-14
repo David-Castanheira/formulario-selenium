ff0655c64a807f899ebb2b53d7891ee0cc554416
David Castanheira de Souza <85026767+David-Castanheira@users.noreply.github.com>
2024-11-14 12:25:22 -0300

    Atualização das informações nos testes de campo

--- form.py
+++ form.py
@@ -12,23 +12,23 @@ local_path = f'file://{os.path.dirname(os.path.realpath(__file__))}'
 driver.get(f'{local_path}/templates/form.html')
 
 nome = driver.find_element(By.NAME, "nome")
-nome.send_keys('Emerson')
+nome.send_keys('David')
 time.sleep(0.8)
 
 ra = driver.find_element(By.NAME, "ra")
-ra.send_keys('2201632')
+ra.send_keys('2202571')
 time.sleep(0.8)
 
 dia = driver.find_element(By.ID, "dia")
-dia.send_keys('25')
+dia.send_keys('05')
 time.sleep(0.8)
 
 mes = driver.find_element(By.ID, "mes")
-mes.send_keys('Novembro')
+mes.send_keys('Agosto')
 time.sleep(0.8)
 
 ano = driver.find_element(By.ID, "ano")
-ano.send_keys('1998')
+ano.send_keys('2003')
 time.sleep(0.8)
 
 ads = driver.find_element(By.ID, "ads")
@@ -36,15 +36,15 @@ ads.click()
 time.sleep(0.8)
 
 avaliacao = driver.find_element(By.ID, "avaliacao")
-avaliacao.send_keys('8')
+avaliacao.send_keys('9')
 time.sleep(0.8)
 
 sugestoes = driver.find_element(By.NAME, "sugestoes")
-sugestoes.send_keys('Sugiro melhorias nos computadores do laboratórios da instituição')
+sugestoes.send_keys('Como forma de aumentar a dinâmica das aulas e o aprendizado, acredito que seria interessante reforçar os principais conceitos teóricos e tornar a aula mais prática')
 time.sleep(0.8)
 
 observacoes = driver.find_element(By.NAME, "observacoes")
-observacoes.send_keys('Obrigado por nos acompanhar esse semestre, professor!')
+observacoes.send_keys('Obrigado por nos acompanhar esse semestre, professor! Pude aprender algumas ferramentas ao longo para adicionar confiabilidade e segurança aos meus projetos. Saiba que será muito útil para o resto da minha carreira profissional.')
 time.sleep(0.8)
 
 botao_feedback = driver.find_element(By.XPATH, "//button[text()='Enviar feedback']")
